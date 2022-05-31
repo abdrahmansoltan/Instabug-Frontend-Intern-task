a348693119d758c25a3e34bcf6b8a9eb79f7614c
Abdelrahman Soltan <abdrahmansoltan98@gmail.com>
2022-05-31 07:37:21 +0200

    finished NotFound page

--- src/pages/NotFound/NotFound.vue
+++ src/pages/NotFound/NotFound.vue
@@ -1,15 +1,23 @@
 <template>
-  <div>
-404
+  <div class="container">
+    <div class="not-found">
+      <img
+        src="@/assets/images/not-found.jpg"
+        alt="not-found image"
+        class="not-found-img"
+      />
+      <p class="not-found-title">404-Page-Not-Found!</p>
+      <p class="not-found-message">
+        Sorry, that page doesn’t exist. What would you like to do?
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
+@import "./NotFound.styles.scss";
 </style>
